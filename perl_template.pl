f70ccd2fa1e569164cd6e1f5468d5c993450e275
Mehdi Kahouadji <mehdi.kahouadji@gmail.com>
2018-12-18 16:14:35 +0100

    Update perl_template.pl

--- perl_template.pl
+++ perl_template.pl
@@ -1,1 +1,21 @@
 
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Template;
+
+
+# output to give to template process
+my $template_vars = {
+    'time' => $time,
+    'points' => \@points
+};
+################################################################################
+# Template output
+################################################################################
+my $template = Template->new({
+    ABSOLUTE => 1,
+});
+copy($template_file, $html_output_file) or die "Copy failed: $!";
+$template->process($html_output_file, $template_vars, $html_output_file) || die $template->error();
